74ee05c6221cb78f7f648c12dad26de878302e09
adibuzz <a_u_z_ipe@yahoo.com>
2026-07-31 22:31:37 -0400

    updated module 1 setup script

--- Module 1 Lab/setup.sh
+++ Module 1 Lab/setup.sh
@@ -1,15 +1,42 @@
-# Install Microsoft VS Code
 #!/bin/bash
 
+# Install Microsoft VS Code
 # Add official rpm repository (published by Microsoft)
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 # Install using dnf directly
-dnf update -y &&
-sudo dnf install code
+sudo dnf update -y &&
+sudo dnf install -y code
 
-# Install pre-requisites before install docker enginer
+
+# Install Terraform
+
+sudo dnf install -y dnf-utils
+
+# Add official rpm repository (published by Hashicorp)
+sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+# Install using dnf directly
+sudo dnf -y install terraform
+
+
+# Aws-ClI install # You could potentially use snap for this one, too.
+sudo dnf install -y curl unzip
+# Download and install the AWS CLI version 2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+# Verify AWS CLI installation
+aws --version
+# Configure AWS CLI with dummy credentials
+aws configure
+aws configure set aws_access_key_id=test
+aws configure set aws_secret_access_key=test
+aws configure set default.region=us-east-1
+aws configure set default.output=json
+
+# Install Docker Engine
+# Install pre-requisites before install docker engine
 sudo dnf install -y dnf-plugins-core
 # Add official rpm repository
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
@@ -21,42 +48,3 @@ sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 # Run docker as non-previliged user
 sudo usermod -aG docker $USER
 newgrp docker
-sudo systemctl restart docker
-
-# Verify Successful Docker Installation
-docker run hello-world
-
-
-# Install Terraform
-#sudo yum install -y yum-utils
-
-sudo dnf install -y dnf-utils
-
-#sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-
-sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo dnf -y install terraform
-
-#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
-
-
-# Aws-ClI install
-sudo dnf install -y curl unzip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-# Verify AWS CLI installation
-aws --version
-
-aws configure
-#test
-#test
-#us-east-1
-
-
-# Install git
-sudo dnf install git-core -y
-
-# Install GitHub CLI
-sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-sudo dnf install gh -y
