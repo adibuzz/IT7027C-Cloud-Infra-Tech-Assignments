534a75d460d2e8c9569f9d2b2d429cbdd8d14bcf
adibuzz <a_u_z_ipe@yahoo.com>
2026-08-21 19:49:27 -0400

    added modern style s3 endpoint declaration to finish it up

--- Module-8-Lab/main.tf
+++ Module-8-Lab/main.tf
@@ -8,17 +8,14 @@ terraform {
     bucket                      = "my-local-cloud-bucket"
     key                         = "terraform.tfstate"
     region                      = "us-east-1"
-    #endpoint                    = "http://localhost:4566"
+    endpoints                    = {s3 = "http://localhost:4566"}
     access_key                  = "test"
     secret_key                  = "test"
-    force_path_style            = true
+    use_path_style            = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
-    endpoints {
-    s3 = "http://localhost:4566"
-  }
   }
 
   required_providers {
