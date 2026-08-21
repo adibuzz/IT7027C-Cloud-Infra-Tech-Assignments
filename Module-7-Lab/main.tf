b313a214419a3255700ab79065f375784f5ac34b
adibuzz <a_u_z_ipe@yahoo.com>
2026-08-21 02:09:27 -0400

    used vault-v2 and updated terminal commands

--- Module-7-Lab/main.tf
+++ Module-7-Lab/main.tf
@@ -1,3 +1,7 @@
+# Before running this terraform script, run the following in your terminal
+# docker run --cap-add=IPC_LOCK -d --name=dev-vault -e 'VAULT_DEV_ROOT_TOKEN_ID=myroot' -p 8200:8200 hashicorp/vault server -dev
+# docker exec -e VAULT_ADDR="http://127.0.0.1:8200" dev-vault vault kv put secret/db config_password="YourSecurePassword123!"
+
 terraform {
   required_providers {
     vault = { source = "hashicorp/vault", version = "~> 3.0" }
@@ -12,8 +16,9 @@ provider "vault" {
 provider "docker" { host = "unix:///var/run/docker.sock" }
 
 # Read the secret
-data "vault_generic_secret" "db_creds" {
-  path = "secret/db"
+data "vault_kv_secret_v2" "db_creds" {
+  mount = "secret"
+  name = "db"
 }
 
 # Use the secret in a container deployment
@@ -21,5 +26,5 @@ resource "docker_image" "postgres" { name = "postgres:13-alpine" }
 resource "docker_container" "db" {
   name  = "secure_db"
   image = docker_image.postgres.image_id
-  env   = ["POSTGRES_PASSWORD=${data.vault_generic_secret.db_creds.data["config_password"]}"]
+  env   = ["POSTGRES_PASSWORD=${data.vault_kv_secret_v2.db_creds.data["config_password"]}"]
 }
