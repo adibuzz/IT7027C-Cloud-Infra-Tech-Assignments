2f37fccb7037259683d73d2e1ed51e6635aeb40f
adibuzz <a_u_z_ipe@yahoo.com>
2026-08-21 04:08:01 -0400

    Added aws as one of the multi-cloud providers

--- Module-11-Lab/main.tf
+++ Module-11-Lab/main.tf
@@ -1,33 +1,75 @@
 terraform {
-
-	required_providers {
-		docker = { source = "kreuzwerker/docker"}
-		kubernetes = {source = "hashicorp/kubernetes"}
-			}
+  required_providers {
+    docker = { 
+      source = "kreuzwerker/docker"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
 }
-
-provider "docker" {host = "unix:///var/run/docker.sock" }
-
-# Added config_context here as well for stability
-
-provider "kubernetes" {
-
-	config_path = "~/.kube/config"
-	config_context = "minikube"
-}
-
 
 # Provider 1: Docker (Simulating On-Prem)
-
-resource "docker_image" "redis" {name = "redis:alpine"}
-
-resource "docker_container" "on_prem_cache" {
-name = "hybrid_redis"
-image = docker_image.redis.image_id
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
 }
 
 # Provider 2: Kubernetes (Simulating Public Cloud)
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "minikube"
+}
 
+# Provider 3: AWS (MiniStack / LocalStack)
+provider "aws" {
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    iam = "http://localhost:4566"
+  }
+}
+
+# --- Resources ---
+
+# Docker Resource
+resource "docker_image" "redis" {
+  name = "redis:alpine"
+}
+
+resource "docker_container" "on_prem_cache" {
+  name  = "hybrid_redis"
+  image = docker_image.redis.image_id
+}
+
+# Kubernetes Resource
 resource "kubernetes_namespace" "hybrid_ns" {
-metadata {name = "hybrid-cloud-ns"}
+  metadata {
+    name = "hybrid-dev" # Corrected from "hybrid-cloud-ns"
+  }
+}
+
+# AWS Resource
+resource "aws_iam_role" "mock_role" {
+  name = "hybrid_dev_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
